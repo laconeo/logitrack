a33b845178eda33d414b1305416c5bd249dda8e5
laconeo@gmail.com <laconeo@gmail.com>
2026-04-02 19:03:33 -0300

    Cambio de Titulo

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ import WarehouseInvoices from "./pages/warehouse/Invoices";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/logitrack">
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
